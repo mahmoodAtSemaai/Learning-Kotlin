0456e0180fac4b8220a8a1c65aec17517010c770
Kashish Luthra <kashish.luthra@semaai.com>
2022-03-30 14:29:18 -0400

    Base Architecture Setup

--- app/src/main/java/com/webkul/mobikul/odoo/core/data/Repository.kt
+++ app/src/main/java/com/webkul/mobikul/odoo/core/data/Repository.kt
@@ -1,4 +1,9 @@
 package com.webkul.mobikul.odoo.core.data
 
+import com.webkul.mobikul.odoo.core.exception.Failure.FeatureFailure
+
+/**
+ * Marker Interface for Repository. Every Repository must implement this.
+ */
 interface Repository {
 }
--- app/src/main/java/com/webkul/mobikul/odoo/core/domain/UseCase.kt
+++ app/src/main/java/com/webkul/mobikul/odoo/core/domain/UseCase.kt
@@ -1,4 +1,7 @@
 package com.webkul.mobikul.odoo.core.domain
 
+/**
+ * Marker Interface for UseCase. Every UseCase must implement this.
+ */
 interface UseCase {
 }
