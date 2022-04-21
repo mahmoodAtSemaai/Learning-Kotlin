6b9769f7fe9fde08041b75ec454a4b6f4ee07f2b
AyanSharma91 <ayansharma00999@gmail.com>
2022-04-21 09:48:39 +0530

    updated_gradle

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,11 +1,11 @@
 object DepVersions {
-    const val buildTools = "3.4.2"
-    const val kotlin = "1.3.41"
+    const val buildTools = "4.2.1"
+    const val kotlin = "1.6.0"
     const val googleServices = "4.3.4"
     const val firebaseCrashlyticsGradle = "2.3.0"
     const val gradleVersion = "2.3.0"
 
-    const val compileSdkVersion = 30
+    const val compileSdkVersion = 31
     const val buildToolsVersion = "28.0.3"
     const val renderScriptTargetApi = 16
     const val targetSdkVersion = 30
