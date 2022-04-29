fe3c37fbb4f1e8166f4f6bbb9c86a146b7172db4
likhit <likhit@udhaar.co>
2022-04-29 23:09:26 +0530

    #Base MVI - Updated IModel.kt with flow for state. - Implemented respective IView and IModel for LoginFragmentV1.kt and LoginViewModel.kt - Extended LoginIntent.kt. LoginState.kt with base.

--- app/src/main/java/com/webkul/mobikul/odoo/core/mvicore/IModel.kt
+++ app/src/main/java/com/webkul/mobikul/odoo/core/mvicore/IModel.kt
@@ -1,14 +1,14 @@
 package com.webkul.mobikul.odoo.core.mvicore
 
-import androidx.lifecycle.LiveData
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.StateFlow
 
 /**
  * Base Mode Interface to be implemented by every viewmodels
  */
 interface IModel<S : IState, I : IIntent> {
     val intents: Channel<I>
-    val state: LiveData<S>
+    val state: StateFlow<S>
 
     fun handlerIntent()
 }
