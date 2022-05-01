f292687cf7d47deaec396d208357f57f09382cc0
Kashish Luthra <kashish.luthra@semaai.com>
2022-05-01 19:33:48 -0400

    SignUp setup

--- app/src/main/java/com/webkul/mobikul/odoo/core/di/RemoteDataSourceModule.kt
+++ app/src/main/java/com/webkul/mobikul/odoo/core/di/RemoteDataSourceModule.kt
@@ -3,6 +3,7 @@ package com.webkul.mobikul.odoo.core.di
 import android.content.Context
 import com.webkul.mobikul.odoo.features.auth.data.remoteSource.AuthServices
 import com.webkul.mobikul.odoo.features.auth.data.remoteSource.LoginRemoteDataSource
+import com.webkul.mobikul.odoo.features.auth.data.remoteSource.SignUpRemoteDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,8 +18,16 @@ object RemoteDataSourceModule {
     @Provides
     @Singleton
     fun loginRemoteDataSource(
-        authServices: AuthServices,
-        @ApplicationContext context: Context
+            authServices: AuthServices,
+            @ApplicationContext context: Context
     ): LoginRemoteDataSource = LoginRemoteDataSource(authServices, context)
+
+
+    @Provides
+    @Singleton
+    fun signUpRemoteDataSource(
+            authServices: AuthServices,
+            @ApplicationContext context: Context
+    ): SignUpRemoteDataSource = SignUpRemoteDataSource(authServices, context)
 
 }
