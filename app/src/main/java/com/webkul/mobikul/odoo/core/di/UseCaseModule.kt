ca8775fd975f859f30a11228a8c24fcfa3dc65c5
Aashishkumar Tathod <aashishtathod@outlook.com>
2022-05-02 11:14:36 +0530

    added ViewPrivacyPolicyUseCase and goToHomePage fun in Login Fragment

--- app/src/main/java/com/webkul/mobikul/odoo/core/di/UseCaseModule.kt
+++ app/src/main/java/com/webkul/mobikul/odoo/core/di/UseCaseModule.kt
@@ -1,11 +1,14 @@
 package com.webkul.mobikul.odoo.core.di
 
 
+import android.content.Context
 import com.webkul.mobikul.odoo.features.auth.domain.repo.LoginRepository
 import com.webkul.mobikul.odoo.features.auth.domain.usecase.LogInUseCase
+import com.webkul.mobikul.odoo.features.auth.domain.usecase.ViewPrivacyPolicyUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,8 +19,13 @@ class UseCaseModule {
     @Provides
     @Singleton
     fun provideLogInUseCase(
-            loginRepository: LoginRepository
+        loginRepository: LoginRepository
     ): LogInUseCase = LogInUseCase(loginRepository)
+
+    @Provides
+    @Singleton
+    fun provideViewPrivacyPolicyUseCase(@ApplicationContext context: Context): ViewPrivacyPolicyUseCase =
+        ViewPrivacyPolicyUseCase(context)
 
 
 }
--- app/src/main/java/com/webkul/mobikul/odoo/features/auth/presentation/LoginState.kt
+++ app/src/main/java/com/webkul/mobikul/odoo/features/auth/presentation/LoginState.kt
@@ -1,5 +1,6 @@
 package com.webkul.mobikul.odoo.features.auth.presentation
 
+import android.content.Intent
 import com.webkul.mobikul.odoo.core.mvicore.IState
 import com.webkul.mobikul.odoo.features.auth.domain.enums.AuthFieldsValidation
 import com.webkul.mobikul.odoo.model.customer.signin.LoginResponse
@@ -7,6 +8,7 @@ import com.webkul.mobikul.odoo.model.customer.signin.LoginResponse
 sealed class LoginState : IState {
     object Idle : LoginState()
     object Loading : LoginState()
+    class PrivacyPolicy(val intent: Intent) : LoginState()
     data class Login(val data: LoginResponse) : LoginState()
     data class InvalidLoginDetailsError(val uiError: AuthFieldsValidation) : LoginState()
     data class Error(val error: String?) : LoginState()
