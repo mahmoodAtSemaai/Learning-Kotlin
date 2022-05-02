3633da2ddd395fe74a8abf675eb9b04fc8760ecc
likhit <likhit@udhaar.co>
2022-05-02 15:49:10 +0530

    #auth_module_signup - Added SignUpRemoteDataSource.kt, SignUpRepository.kt and SignUpRepositoryImpl.kt - Updated RepositoryModule.kt

--- app/src/main/java/com/webkul/mobikul/odoo/core/di/RepositoryModule.kt
+++ app/src/main/java/com/webkul/mobikul/odoo/core/di/RepositoryModule.kt
@@ -2,8 +2,11 @@ package com.webkul.mobikul.odoo.core.di
 
 import com.webkul.mobikul.odoo.core.data.local.AppPreferences
 import com.webkul.mobikul.odoo.features.auth.data.remoteSource.LoginRemoteDataSource
+import com.webkul.mobikul.odoo.features.auth.data.remoteSource.SignUpRemoteDataSource
 import com.webkul.mobikul.odoo.features.auth.data.repo.LoginRepositoryImpl
+import com.webkul.mobikul.odoo.features.auth.data.repo.SignUpRepositoryImpl
 import com.webkul.mobikul.odoo.features.auth.domain.repo.LoginRepository
+import com.webkul.mobikul.odoo.features.auth.domain.repo.SignUpRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,4 +24,11 @@ class RepositoryModule {
             remoteDataSource: LoginRemoteDataSource,
             appPreferences: AppPreferences
     ): LoginRepository = LoginRepositoryImpl(remoteDataSource,appPreferences)
+
+    @Provides
+    @Singleton
+    fun provideSignUpRepository(
+        remoteDataSource: SignUpRemoteDataSource,
+        appPreferences: AppPreferences
+    ): SignUpRepository = SignUpRepositoryImpl(remoteDataSource,appPreferences)
 }
