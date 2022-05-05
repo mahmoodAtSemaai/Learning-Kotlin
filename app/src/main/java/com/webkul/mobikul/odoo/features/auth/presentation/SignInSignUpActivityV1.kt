5b19ae785df586b3fe2cac1e1f33d452d1c80650
Kashish Luthra <kashish.luthra@semaai.com>
2022-05-05 14:13:32 -0400

    Resolving PR comments

--- app/src/main/java/com/webkul/mobikul/odoo/features/auth/presentation/SignInSignUpActivityV1.kt
+++ app/src/main/java/com/webkul/mobikul/odoo/features/auth/presentation/SignInSignUpActivityV1.kt
@@ -8,45 +8,39 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SignInSignUpActivityV1 @Inject constructor(): BindingBaseActivity<ActivitySignInSignUpV1Binding>() {
+class SignInSignUpActivityV1 @Inject constructor() : BindingBaseActivity<ActivitySignInSignUpV1Binding>() {
 
     override val contentViewId: Int
         get() = R.layout.activity_sign_in_sign_up_v1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setOnClickListeners()
-
     }
 
     private fun setOnClickListeners() {
-
         binding.loginButton.setOnClickListener { openLogin() }
-
         binding.signUpButton.setOnClickListener { openSignUp() }
-
         binding.fingerprintButton.setOnClickListener { }
     }
 
 
     private fun openLogin() {
         supportFragmentManager.beginTransaction()
-            .add(
-                R.id.fragment_container_v1,
-                LoginFragmentV1.newInstance(),
-                LoginFragmentV1::class.java.simpleName
-            ).commit()
+                .add(
+                        R.id.fragment_container_v1,
+                        LoginFragmentV1.newInstance(),
+                        LoginFragmentV1::class.java.simpleName
+                ).commit()
     }
 
     private fun openSignUp() {
         supportFragmentManager.beginTransaction()
-            .add(
-                R.id.fragment_container_v1,
-                SignUpFragmentV1.newInstance(),
-                SignUpFragmentV1::class.java.simpleName
-            ).commit()
+                .add(
+                        R.id.fragment_container_v1,
+                        SignUpFragmentV1.newInstance(),
+                        SignUpFragmentV1::class.java.simpleName
+                ).commit()
     }
-
 
 }
