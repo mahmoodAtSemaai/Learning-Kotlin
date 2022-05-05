84fe627c1d3e48c32065f172338c1272ecc77fbd
Kashish Luthra <kashish.luthra@semaai.com>
2022-05-05 14:15:03 -0400

    Resolving PR comments

--- app/src/main/java/com/webkul/mobikul/odoo/features/auth/data/remoteSource/LoginRemoteDataSource.kt
+++ app/src/main/java/com/webkul/mobikul/odoo/features/auth/data/remoteSource/LoginRemoteDataSource.kt
@@ -6,11 +6,10 @@ import com.webkul.mobikul.odoo.core.data.remote.BaseRemoteDataSource
 import com.webkul.mobikul.odoo.model.request.RegisterDeviceTokenRequest
 import javax.inject.Inject
 
-class LoginRemoteDataSource @Inject constructor(private val apiService: AuthServices,private val context: Context) : BaseRemoteDataSource() {
+class LoginRemoteDataSource @Inject constructor(private val apiService: AuthServices, private val context: Context) : BaseRemoteDataSource() {
 
     suspend fun logIn() = safeApiCall {
         Log.e("testing server", "request body ${RegisterDeviceTokenRequest(context).toString()}")
         apiService.signIn(RegisterDeviceTokenRequest(context).toString())
-
     }
 }
