0622bd18593817fb55362096b22d09100ac922d3
MSI KATANA <mohammed.mahmood@semaai.com>
2022-10-20 01:48:47 +0530

    ##Search UI/UX improvement 1)data mapper added to domain layer

---
+++ app/src/main/java/com/webkul/mobikul/odoo/ui/learning_anko/domain/mapper/DataMapper.kt
@@ -0,0 +1,37 @@
+package com.webkul.mobikul.odoo.ui.learning_anko.domain.mapper
+
+import com.webkul.mobikul.odoo.ui.learning_anko.data.Forecast
+import com.webkul.mobikul.odoo.ui.learning_anko.data.ForecastResult
+import com.webkul.mobikul.odoo.ui.learning_anko.data.entity.ForecastEntity
+import com.webkul.mobikul.odoo.ui.learning_anko.data.entity.ForecastListEntity
+import java.text.DateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class DataMapper {
+
+	fun convertFromDataModel(forecast: ForecastResult): ForecastListEntity {
+		return ForecastListEntity(forecast.city.name,
+			forecast.city.country,
+			convertForecastListToDomain(forecast.list))
+	}
+
+	private fun convertForecastListToDomain(list: List<Forecast>): List<ForecastEntity> {
+		return list.mapIndexed { i, forecast ->
+			val dt = Calendar.getInstance().timeInMillis + TimeUnit.DAYS.toMillis(i.toLong())
+			convertForecastItemToDomain(forecast.copy(dt = dt))
+		}
+	}
+
+	private fun convertForecastItemToDomain(forecast: Forecast): ForecastEntity {
+		return ForecastEntity(convertDate(forecast.dt), forecast.weather[0].description,
+			forecast.temp.max.toInt(), forecast.temp.min.toInt())
+	}
+
+	private fun convertDate(date: Long): String {
+		val df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
+		return df.format(date)
+	}
+
+
+}
